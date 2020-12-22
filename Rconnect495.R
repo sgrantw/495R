d33782b67b5f519187adbb797cdfd4a96daa0f6f
sgrantw <76459064+sgrantw@users.noreply.github.com>
2020-12-21 18:32:02 -0700

    Add files via upload

---
+++ Rconnect495.R
@@ -0,0 +1,8 @@
+rsconnect::setAccountInfo(name='sgrantw',
+                          token='B2F5FA8742B5B4CE38C01AFEBFA8E3FE',
+                          secret='LB0WRiKtAbPqh4DOr61+RLozouGWzafjzxrlkCnD')
+
+rsconnect::deployApp("/Users/grantspangenberg/BYU/Stat495Project-Nba EFG.Rmd")
+
+
+
